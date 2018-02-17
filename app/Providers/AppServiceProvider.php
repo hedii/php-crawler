0c2b3cb5214b37392c071d3404ebe61ff64e0394
hedii <contact@hedichaibi.com>
2018-02-17 12:57:53 +0100

    Upgrade Laravel to 5.6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::doubleEncode();
+        //
     }
 
     /**
